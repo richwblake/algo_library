de67e4d2394aaf6b25338f5a8490478869e1ea25
wills blake <richwblake@gmail.com>
2022-03-10 21:05:03 -0800

    added instructions and jsdoc to two_sock

---
+++ scripts/two_sock.js
@@ -0,0 +1,14 @@
+// Write your code inside the function below
+// Run the test for this file with the command: npm run two_sock
+
+/* 
+  Kaelah is getting ready to go to the store. Before she can put her shoes on, she needs socks!
+  Write an algorithm that takes in a collection (array) of colored socks, and return the color of
+  the first sock pair that you find.
+
+  @param {Array} n - The array of socks to search through
+  @return {String} - The value of the color of the first sock pair that is found
+*/
+export default two_sock = (n) => {
+
+};
