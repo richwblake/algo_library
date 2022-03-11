bf0395bddde6bd5cc678bb2e87a4058da61b044c
wills blake <richwblake@gmail.com>
2022-03-10 21:11:45 -0800

    renamed config to two_sock.test.js

--- scripts/two_sock.js
+++ scripts/two_sock.js
@@ -4,11 +4,11 @@
 /* 
   Kaelah is getting ready to go to the store. Before she can put her shoes on, she needs socks!
   Write an algorithm that takes in a collection (array) of colored socks, and return the color of
-  the first sock pair that you find.
+  the first sock pair present in the collection.
 
   @param {Array} n - The array of socks to search through
   @return {String} - The value of the color of the first sock pair that is found
 */
 export default two_sock = (n) => {
-
+  
 };
---
+++ tests/two_sock.test.js
@@ -0,0 +1,5 @@
+import two_sock from "../scripts/two_sock";
+
+test('Function exists', () => {
+  expect(two_sock().toBe(undefined));
+});
